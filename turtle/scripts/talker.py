afebc97d7643c26b6c7dc29e4e7503d4cff6c8fb
Mateusz Scislowski <Matis19993@wp.pl>
2020-03-13 13:20:09 +0100

    Ukonczony projekt nr 1 - sterowanie zolwiem

--- turtle/scripts/talker.py
+++ turtle/scripts/talker.py
@@ -5,35 +5,41 @@ from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 import sys, select, termios, tty
 
-def getKey():
+def getKey():     #funkcja zwracajaca wcisniety klawisz, niestety nie udalo nam sie utworzyc prostszej
     tty.setraw(sys.stdin.fileno())
     select.select([sys.stdin], [], [], 0)
     key = sys.stdin.read(1)
     termios.tcsetattr(sys.stdin, termios.TCSANOW, termios.tcgetattr(sys.stdin.fileno()))
     return key
 
-def talker():
-    pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
+def talker():     #funkcja publikujaca wiadomosci do wezla zolwia
+    pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)    #ustawienie podstawowych atrybutow
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(1000)
-    twist = Twist()
+    rate = rospy.Rate(100)
+    twist = Twist()    #utworzenie obiektu wiadomosci
     while not rospy.is_shutdown():
-        keycode = getKey()
+        keycode = getKey()    #odczytanie i przypisanie wcisnietego klawisza
+        #sprawdzenie, czy wcisniety klawisz odpowiada ktoremus z ustawionych w parametrach
+        #jesli tak, to w zaleznosci od klawisza, polozenie zolwia zmienia sie liniowo badz biegunowo
         if keycode == rospy.get_param("/forward"):
             twist.linear.x = 2
             twist.angular.z = 0
-        if keycode == rospy.get_param("/back"):
+        elif keycode == rospy.get_param("/back"):
             twist.linear.x = -2
             twist.angular.z = 0
-        if keycode == rospy.get_param("/left"):
+        elif keycode == rospy.get_param("/left"):
             twist.angular.z = 2
             twist.linear.x = 0
-        if keycode == rospy.get_param("/right"):
+        elif keycode == rospy.get_param("/right"):
             twist.angular.z = -2
             twist.linear.x = 0
-        pub.publish(twist)
+        #jesli nie, to polozenie zolwia nie ulega zmianie
+        else:
+            twist.linear.x = 0
+            twist.angular.z = 0
+        pub.publish(twist)    #publikacja wiadomosci
 
-if __name__ == '__main__':
+if __name__ == '__main__':    #uzycie funkcji publikujacej wiadomosci
     try:
         talker()
     except rospy.ROSInterruptException:
